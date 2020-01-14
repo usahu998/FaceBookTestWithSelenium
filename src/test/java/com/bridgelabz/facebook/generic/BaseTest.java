db57edadcaa7cb93fe80756b3574c9bd9b588691
Upendra <usahu998@gmail.com>
2020-01-14 19:38:27 +0530

    adding generic classes

---
+++ src/test/java/com/bridgelabz/facebook/generic/BaseTest.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.facebook.generic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.*;
+
+public class BaseTest {
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        System.setProperty("webdriver.chrome.driver", "/home/admin1/SeleniumTesting/SeleniumUpcastingConcept/src/Driver/chromedriver");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void close() {
+        driver.close();
+    }
+}
---
+++ src/test/java/com/bridgelabz/facebook/generic/IAutoConstant.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.facebook.generic;
+
+public interface IAutoConstant {
+
+    String CONFIG_PATH = "./config.properties";
+    String EXCEL_PATH = "./testdata/data.xlsx";
+    String GECKO_KEY = "webdriver.gecko.driver";
+    String GECKO_VALUE = "./Driver/geckodriver";
+    String CHROME_KEY = "webdriver.chrome.driver";
+    String CHROME_VALUE = "./Driver/chromedriver";
+
+}
