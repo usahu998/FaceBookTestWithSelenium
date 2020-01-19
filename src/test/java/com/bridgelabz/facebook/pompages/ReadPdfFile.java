7a73253bf9c5c7ae2f3d4eeb6d1902cb1ff49fb5
Upendra Sahu <usahu998@gmail.com>
2020-01-20 03:14:28 +0530

    Adding Read pdf file

---
+++ src/test/java/com/bridgelabz/facebook/pompages/ReadPdfFile.java
@@ -0,0 +1,38 @@
+package com.bridgelabz.facebook.pompages;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.PDFTextStripperByArea;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ReadPdfFile {
+    public static void main(String[] args) throws IOException {
+
+        try (PDDocument document = PDDocument.load(new File("C:\\Users\\user\\IdeaProjects\\FaceBookTestWithSelenium\\src\\test\\java\\com\\bridgelabz\\facebook\\files\\sample.pdf"))) {
+
+            document.getClass();
+
+            if (!document.isEncrypted()) {
+
+                PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+                stripper.setSortByPosition(true);
+
+                PDFTextStripper tStripper = new PDFTextStripper();
+
+                String pdfFileInText = tStripper.getText(document);
+                //System.out.println("Text:" + st);
+
+                // split by whitespace
+                String lines[] = pdfFileInText.split("\\r?\\n");
+                for (String line : lines) {
+                    System.out.println(line);
+                }
+
+            }
+
+        }
+
+    }
+}
